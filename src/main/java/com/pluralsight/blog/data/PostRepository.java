e1998af60b4ca97d0a80f0309af67542f8b8dbd3
Andrew Oluoch <oluoch3@gmail.com>
2020-06-12 15:47:08 -0400

    pluralsight blog

---
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -0,0 +1,17 @@
+package com.pluralsight.blog.data;
+
+import com.pluralsight.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+
+@RestResource
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+    @RestResource(rel="contains-title", path="containsTitle")
+    List<Post> findByTitleContaining(String title);
+
+    List<Post>findByAuthor_Lastname(String lastname);
+}
